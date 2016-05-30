6f2ffcd7fb3cac8d562eb819f0046e5689d40bd3
creimers <christoph@superservice-international.com>
2016-05-30 16:54:51 +0200

    successfully display image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-export default class Library {
-  constructor() {
-    this._name = 'Library';
+import 'croppie/croppie.css';
+let croppie = require('croppie/croppie.js');
+
+const MODULE_NAME = 'ngImageCrop';
+
+export default MODULE_NAME;
+
+const module = angular.module(MODULE_NAME, []);
+
+class ImageCropDirective {
+
+  constructor($window) {
+    this.$window = $window;
+    this.restrict = 'E';
+    this.template = require('./template.jade')();
+    this.scope = {
+      originalImage: '<',
+      croppedImage: '='
+    };
   }
-  get name() {
-    return this._name;
+
+  link(scope, element) {
+
+    let c = new croppie(element[0]);
+ 
+    scope.$watch('originalImage', ()=> {
+      console.log('original image changed.');
+      if (scope.originalImage !== undefined) {
+        let reader = new FileReader();
+
+        reader.onload = ()=> {
+          c.bind(reader.result);
+        };
+        reader.readAsDataURL(scope.originalImage);
+      }
+    });
   }
+
 }
+
+module.directive('imageCrop', () => new ImageCropDirective());
