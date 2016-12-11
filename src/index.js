fde4f695066bd018c6529a992a3db50322ac67e3
Christoph Reimers <christoph@superservice-international.com>
2016-12-11 18:19:19 +0100

    built with manual dependency injection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default MODULE_NAME;
 const module = angular.module(MODULE_NAME, []);
 
 class ImageCropDirectiveCtrl {
-  /* @ngInject */
+
   constructor($scope, $document, $element, $timeout, $window) {
     this.$document = $document;
     this.$element = $element;
@@ -95,6 +95,8 @@ class ImageCropDirectiveCtrl {
     return blob;
   }
 }
+
+ImageCropDirectiveCtrl.$inject = ['$scope', '$document', '$element', '$timeout', '$window'];
 
 class ImageCropDirective {
 
